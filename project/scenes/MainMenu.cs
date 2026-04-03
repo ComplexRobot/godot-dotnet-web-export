1510129cd340092505fa5643d44c797ab8d0277f
ComplexRobot <complexrobotx@gmail.com>
2026-04-02 21:06:18 -0700

    Added test code for #5 and #6.

--- project/scenes/MainMenu.cs
+++ project/scenes/MainMenu.cs
@@ -1,5 +1,13 @@
 using Godot;
+using Newtonsoft.Json;
 using System;
+using System.Globalization;
+
+public class TestData
+{
+    public string Name;
+    public int Value;
+}
 
 public partial class MainMenu : Control
 {
@@ -11,6 +19,18 @@ public partial class MainMenu : Control
 
     public override void _Ready()
     {
+        float value = 123.45f;
+        string str = value.ToString(CultureInfo.InvariantCulture);
+        GD.Print(str);   // Safe
+        GD.Print(value); // Crashes
+
+        var obj = new TestData { Name = "Test", Value = 42 };
+        string json = JsonConvert.SerializeObject(obj);   // Works in editor / desktop
+        var back = JsonConvert.DeserializeObject<TestData>(json); // Aborts in Web
+
+        GD.Print(back);
+        GD.Print(json);
+
         _mainScene = ResourceLoader.Load<PackedScene>("res://main.tscn");
         GetNode<Button>("VBoxContainer/Cube").Pressed += () => ChangeToMainAndSpawn(Cube.Instantiate());
         GetNode<Button>("VBoxContainer/Sphere").Pressed += () => ChangeToMainAndSpawn(Sphere.Instantiate());
